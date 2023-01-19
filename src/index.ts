2e08cf49214b31a61af469a90726f161e20c5e88
Zach King <zacheking10@gmail.com>
2023-01-19 02:47:35 -0700

    [feature]: add logic flow comments

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,20 @@ interface BookResponseData {
   authors: number[];
 }
 
+// Cases:
+// - A book with no author is found => return book title and book description
+// - A book with author(s) is found => return book title, description, and author(s)
+// - No book is found => return "No book found."
+
+// Steps:
+// 1. Accept input from user (Book Title)
+// 2. Verify which of the above cases was met and respond accordingly
+// 3. If there is more than one author their names must be parsed and formatted properly
+// 4. Loop recursively
+
 class Controller {
+  // Define a method that can be used to search book titles, it must return the book title, description, and an array of the authors IDs, if no data is found return
+  // null so that the recursive function can handle logic flow accordingly
   static async searchBookTitle(
     title: string
   ): Promise<BookResponseData | null> {
@@ -37,15 +50,20 @@ class Controller {
         return null;
       });
   }
+  // Define a method that can be used to search author names by author ID, it must return the author names in plain english format
   static async getAuthorData(authorIDs: number[]): Promise<string[]> {
     let authorNames: string[] = [];
     for (const authorID of authorIDs) {
+      // loop through the array of author IDs
       await axios.get(`${baseURL}authors/${authorID}`).then((res) => {
-        const { firstName, middleInitial, lastName } = res.data;
+        // make an async request to :authorId endpoint
+        const { firstName, middleInitial, lastName } = res.data; // destructure the names singular parts
+        // If the author has a middle name we return all 3 parts with a single space inbetween them, otherwise we return just the first and last name with a space between
+        // we do it this way to avoid having a name returned as "First undefined Last"
         const authorFullName = !middleInitial
           ? `${firstName} ${lastName}`
           : `${firstName} ${middleInitial} ${lastName}`;
-        authorNames.push(authorFullName);
+        authorNames.push(authorFullName); // push the formatted name into an array so they can all be returned together after the loop is complete
       });
     }
     return authorNames;
@@ -56,6 +74,7 @@ const recursiveReadline = function (): void {
   reader.question("Search for a book? \n", async (input) => {
     const bookData = await Controller.searchBookTitle(input);
     if (bookData === null) {
+      // If the controller function returned null it means there was no data found, so we can error handle and loop
       console.log(red("No book found."));
       return recursiveReadline();
     }
@@ -68,8 +87,11 @@ const recursiveReadline = function (): void {
       console.log(blue(`${bookData.title}\n${bookData.description}`));
     }
 
-    return recursiveReadline();
+    // Seperate the above cases so that all of the data is displayed at once rather than with a delay. If we log the title and description independently then there is
+    // some delay between the the title/desc being logged and the author names being logged ( since we're waiting for up to two more async calls )
+
+    return recursiveReadline(); // loop again
   });
 };
 
-recursiveReadline();
+recursiveReadline(); // Intitial invocation of our main function
